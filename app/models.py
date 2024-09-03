26b786fcd7ee9ad0c5576f72b3ad23dbc7049ea1
britooyg <matheus.britomcma@gmail.com>
2024-09-03 20:25:59 -0300

    model atualizado

--- app/models.py
+++ app/models.py
@@ -1,18 +1,21 @@
 class Cliente:
-    def __init__(self, nome_cliente, cpf, endereco, telefone, email ):
+    def __init__(self, nome_cliente, cpf, endereco, telefone, email, data_nascimento):
         self.nome_cliente = nome_cliente
         self.cpf = cpf
         self.endereco = endereco
         self.telefone = telefone
         self.email = email
+        self.data_nascimento = data_nascimento
 
 class Barbeiro:
-    def __init__(self, nome_barbeiro, cpf, email, apelido, especialidade):
+    def __init__(self, nome_barbeiro, cpf, email, apelido, especialidade, data_nascimento, telefone):
         self.nome_barbeiro = nome_barbeiro
         self.cpf = cpf
         self.email = email
         self.apelido = apelido
         self.especialidade = especialidade
+        self.data_nascimento = data_nascimento
+        self.telefone = telefone
 
 
 
