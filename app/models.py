e496f831ee654e5a4687db18279b476e101e0cb8
Victor Fernando David Silva <2300870@SALAS.AULAS>
2024-09-03 21:59:13 -0300

    Cadastro barbeiro 100% ATUALIZADO (campos do fotmulário)

--- app/models.py
+++ app/models.py
@@ -1,7 +1,23 @@
 class Cliente:
-    def __init__(self, nome_completo, cpf, endereco, apelido, especialidade):
-        self.nome_completo = nome_completo
+    def __init__(self, nome_cliente, cpf, endereco, telefone, email, data_nascimento):
+        self.nome_cliente = nome_cliente
         self.cpf = cpf
         self.endereco = endereco
+        self.telefone = telefone
+        self.email = email
+        self.data_nascimento = data_nascimento
+
+class Barbeiro:
+    def __init__(self, nome_barbeiro, cpf, email, apelido, especialidade, data_nascimento, telefone):
+        self.nome_barbeiro = nome_barbeiro
+        self.cpf = cpf
+        self.email = email
         self.apelido = apelido
         self.especialidade = especialidade
+        self.data_nascimento = data_nascimento
+        self.telefone = telefone
+
+
+
+
+
